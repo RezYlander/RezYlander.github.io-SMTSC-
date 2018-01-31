5df38711ed59b4368e28da4b1652ccab38028fe7
Filip Witkowski <rezylander@gmail.com>
2018-01-31 11:58:47 +0100

    minor change

--- src/app.jsx
+++ src/app.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import './main.scss';
 import {Game} from "./game.jsx";
 import Sound from "react-sound";
-let m = require('./sound/music.mp3');
+let music = require('./sound/music.mp3');
 
 
 class App extends React.Component {
     render() {
         return <div>
             <Sound
-                    url={m}
+                    url={music}
                     playStatus={Sound.status.PLAYING}
                     playFromPosition={0 /* in milliseconds */}
                     loop={true}
